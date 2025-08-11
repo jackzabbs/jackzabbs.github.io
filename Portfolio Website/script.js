50135127beaedf8d232302792136be26008d4a96
jackzabbs <jdzabbs@gmail.com>
2025-08-11 09:29:44 +0100

    Add files via upload

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -1,7 +1,17 @@
 const images = [
-  'images/photo1.jpg',
-  'images/photo2.jpg',
-  'images/photo3.jpg'
+  'Bedroom 1 Shelf removed.jpg',
+  'draft-2.jpg',
+  'Passage 8-20.jpg',
+  'White Noise Scarf-06.jpg',
+  'White Noise Scarf-05.jpg',
+  'Tukki-19.jpg',
+  'Tukki-18.jpg',
+  'Tukki-4.jpg',
+  'Tree Hang - Grain.jpg',
+  'Sonnie Ghost Control Photos.jpg',
+  'Sonnie Ghost Control Photos-2.jpg',
+  'Passage 8-13.jpg',
+  'Passage 8-8.jpg',
 ];
 
 let current = 0;
